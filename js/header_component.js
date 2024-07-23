bcd2a5f29e2e2f94bb95aab03214e31487754f1a
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2024-07-23 15:52:37 +0900

    fix : header 部分のデザイン修正

--- js/header_component.js
+++ js/header_component.js
@@ -17,13 +17,15 @@ class HeaderSection extends HTMLElement {
 		const shadow = this.attachShadow({ mode: "open" });
 		shadow.innerHTML = `
 			<header>
-			${
-				this.path === "root"
-					? "守山歴史探訪"
-					: this.path === "article"
-					? '<a href="../../index.html">守山歴史探訪</a>'
-					: '<a href="../index.html">守山歴史探訪</a>'
-			}
+				<p>
+					${
+						this.path === "root"
+							? "守山歴史探訪"
+							: this.path === "article"
+							? '<a href="../../index.html">守山歴史探訪</a>'
+							: '<a href="../index.html">守山歴史探訪</a>'
+					}
+				</p>
 			</header>
 
 			<style>
@@ -33,9 +35,9 @@ class HeaderSection extends HTMLElement {
 					left: 0;
 					box-sizing: border-box;
 					width: 100dvw;
-					max-width: 1300px;
 					display: flex;
 					justify-content: flex-start;
+					align-items: left;
 					padding: 20px 30px 15px;
 					margin: 0;
 					-webkit-backdrop-filter: blur(10px);
@@ -43,13 +45,21 @@ class HeaderSection extends HTMLElement {
 					background-color: rgba(177, 212, 247, 0.7);
 					z-index: 10;
 				}
+				
+				header p {
+					text-align: left;
+					width: 100%;
+					max-width: 1300px;
+					margin: 0 auto;
+				}
 
-				header a {
+				header p a {
 					text-decoration: none;
 					color: #000;
 					font-size: 20px;
 					line-height: 30px;
 					font-weight: 700;
+					display: block;
 				}
 			</style>
 		`;
