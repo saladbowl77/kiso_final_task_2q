336e95bf64b638779e34997f28f5788ca8bcee50
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2024-07-23 15:21:27 +0900

    fix : WebComponent内のリンクにも対応

--- js/smooth_move.js
+++ js/smooth_move.js
@@ -1,7 +1,6 @@
 // 同一ドメインからの遷移のみアニメーションを再生。
 // アニメーションのチラつき防止のためデフォルトで要素表示
 const movedFromPage = document.getElementById("movedFromPageWrap");
-console.log(document.referrer.indexOf(window.location.hostname) !== -1);
 if (document.referrer.indexOf(window.location.hostname) !== -1) {
 	document.body.classList.add("noMove");
 	setTimeout(() => {
@@ -12,23 +11,40 @@ if (document.referrer.indexOf(window.location.hostname) !== -1) {
 	movedFromPage.innerHTML = "";
 }
 
-const aTags = document.getElementsByTagName("a");
 const moveToPage = document.getElementById("moveToPageWrap");
+const addLinkEvent = (aTags) => {
+	for (const a of aTags) {
+		a.addEventListener("click", (e) => {
+			// 遷移先が同一ドメインでない場合はモーションをつけずにreturn
+			if (a.href.indexOf(window.location.hostname) === -1) return;
+			e.preventDefault();
 
-for (const a of aTags) {
-	console.log(a);
-	a.addEventListener("click", (e) => {
-		e.preventDefault();
+			moveToPage.insertAdjacentHTML(
+				"afterbegin",
+				`
+					<div class="wave"></div>
+				`
+			);
 
-		moveToPage.insertAdjacentHTML(
-			"afterbegin",
-			`
-				<div class="wave"></div>
-			`
-		);
+			setTimeout(() => {
+				window.location.href = a.href;
+			}, 2000);
+		});
+	}
+};
 
-		setTimeout(() => {
-			window.location.href = a.href;
-		}, 2000);
-	});
+addLinkEvent(document.getElementsByTagName("a"));
+
+if (document.getElementsByTagName("header-section").length > 0) {
+	addLinkEvent(
+		document
+			.getElementsByTagName("header-section")[0]
+			.shadowRoot.querySelectorAll("header a")
+	);
 }
+
+addLinkEvent(
+	document
+		.getElementsByTagName("footer-section")[0]
+		.shadowRoot.querySelectorAll("footer a")
+);
